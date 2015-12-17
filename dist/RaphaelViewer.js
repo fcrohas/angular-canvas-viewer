7210f8de6ded9adcd74dadbafdaf318b4c3200bc
Fabrice Crohas <fcrohas@gmail.com>
2015-12-17 01:23:51 +0100

    Fix height , beautify theme

--- dist/RaphaelViewer.js
+++ dist/RaphaelViewer.js
@@ -12,7 +12,7 @@ angular.module('RaphaelViewer',[]).directive('imageViewer', ['$window', function
 		// controller: function($scope, $element, $attrs, $transclude) {},
 		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
 		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
-		template: '<div class="viewer"'+ 
+		template: '<div class="viewer-container"><div class="viewer" '+ 
 				'width="100%" height="100%"'+  
 				'ng-mouseleave="canMove=false"'+ 
 				'ng-mousedown="mousedown($event)"'+ 
@@ -25,15 +25,19 @@ angular.module('RaphaelViewer',[]).directive('imageViewer', ['$window', function
 				'<div class="btn btn-info" ng-click="rotateright()"><i class="fa fa-rotate-right"></i></div>'+
 				'<div class="btn btn-info" ng-click="zoomout()"><i class="fa fa-search-minus"></i></div>'+
 				'<div class="btn btn-info" ng-click="zoomin()"><i class="fa fa-search-plus"></i></div>'+
-				'</div>'+
+				'</div></div>'+
 		'</div>',
 		// templateUrl: '',
 		// replace: true,
 		// transclude: true,
 		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
 		link: function($scope, iElm, iAttrs, controller) {
-			var	raphaelEl = iElm.find('div')[0];
-			var paper = Raphael(raphaelEl, $window.width, $window.height);
+			var	raphaelEl = iElm.find('div')[1];
+			// look for 
+			var inNode = angular.element(iElm.find('div')[0])[0];
+			directiveParentNode = inNode.parentNode.parentNode;
+			console.log(angular.element(directiveParentNode));
+			var paper = Raphael(raphaelEl, directiveParentNode.clientWidth-40, directiveParentNode.clientHeight-10);
 			var img = null;
 			var zoom = 1.0;
 			var rotate = 0;
